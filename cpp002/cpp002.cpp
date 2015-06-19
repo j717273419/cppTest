19ce388c8bddd6cd0c0b3736ffcd6b8edda8fa4a
j717273419 <717273419@qq.com>
2015-06-19 01:29:43 -0700

    cpp putchar

---
+++ cpp002/cpp002.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "stdio.h"
+using namespace std;
+int main()
+{
+    char a,b,c;
+    a='B';b='O';c='Y';
+    putchar(a);
+    putchar(b);
+    putchar(c);
+    putchar('\n');
+    return 0;
+}
